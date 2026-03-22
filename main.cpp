b37b027e9a7ea1a362277a008920faedb8470b2a
tommyrobot666 <tommyrobot10@gmail.com>
2026-03-22 01:37:55 -0700

    draw rectangle

--- main.cpp
+++ main.cpp
@@ -43,10 +43,15 @@ int main() {
     unsigned int shaderProgram = setUpShaders();
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };  
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    }; 
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -58,7 +63,11 @@ int main() {
     //GL_STATIC_DRAW is for unchanging things, GL_DYNAMIC_DRAW is for changing things
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
 
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //GL_STATIC_DRAW is for unchanging things, GL_DYNAMIC_DRAW is for changing things
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // tell it how vertices is formatted
     glEnableVertexAttribArray(0);
@@ -95,7 +104,8 @@ void renderFrame(GLFWwindow *window, unsigned int VAO, unsigned int shaderProgra
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0); // i think this is the reason why glBindVertexArray(VAO) gets called again
 }
 
 void processInput(GLFWwindow *window)
