4f09ad2ab4ce8a833e58b8affc8d459bf0d92040
tommyrobot666 <tommyrobot10@gmail.com>
2026-07-19 17:39:47 -0700

    add all the other directions

--- src/ereacore/Direction.h
+++ src/ereacore/Direction.h
@@ -4,8 +4,13 @@
 
 namespace Direction {
     constexpr glm::ivec3 UP = glm::vec3(0,1,0);
+    constexpr glm::ivec3 DOWN = glm::vec3(0,-1,0);
+    constexpr glm::ivec3 WEST = glm::vec3(1,0,0);
+    constexpr glm::ivec3 EAST = glm::vec3(-1,0,0);
+    constexpr glm::ivec3 NORTH = glm::vec3(0,0,1);
+    constexpr glm::ivec3 SOUTH = glm::vec3(0,0,-1);
 
     constexpr int ALL_SIZE = 6;
-    const glm::ivec3* ALL = new glm::ivec3[ALL_SIZE]{UP,UP,UP,UP,UP,UP,};
+    inline const glm::ivec3* ALL = new glm::ivec3[ALL_SIZE]{UP,DOWN,WEST,EAST,NORTH,SOUTH,};
     const std::vector<glm::ivec3> ALL_VEC = {ALL,ALL+ALL_SIZE};
 }
